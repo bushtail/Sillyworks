41e5a955bd36e4c072e2e43f29fb0e78c4976426
bushtail <evannosich@icloud.com>
2026-09-24 22:22:56 -0400

    trader prep, ppsh45 prep too

--- Sillyworks.Server/Sillyworks.cs
+++ Sillyworks.Server/Sillyworks.cs
@@ -2,15 +2,22 @@
 using JetBrains.Annotations;
 using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Common;
+using WTTServerCommonLib.Models;
 
 namespace Sillyworks.Server;
 
 [Injectable(TypePriority = OnLoadOrder.Preload + 1), UsedImplicitly]
 public class Sillyworks(WTTServerCommonLib.WTTServerCommonLib wttServerCommonLib) : IOnLoad
 {
+    internal readonly MongoId traderId = new("6ab5da734a1b005364c19e9d");
+    
     public async Task OnLoadAsync(CancellationToken cancellationToken)
     {
         var assembly = Assembly.GetExecutingAssembly();
+        
+        TraderIds.Add("MILO", traderId);
+        
         await wttServerCommonLib.CustomItemServiceExtended.CreateCustomItems(assembly);
     }
 }
